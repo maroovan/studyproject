3473e09249cea8ca9135a62722f040662fb8d60f
Leemvale <neuromiwork@gmail.com>
2017-11-08 15:30:35 +0300

    'header-component'

--- front/Musicat/musicat/src/app/app.module.ts
+++ front/Musicat/musicat/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserPageComponent } from './user-page/user-page.component';
+import { ButtonComponent } from './button/button.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserPageComponent,
+    ButtonComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ front/Musicat/musicat/src/app/user.ts
@@ -0,0 +1,12 @@
+export class User {
+  id: number;
+  name: string;
+  subscriptions_number: number;
+  subscriptions: object[];
+  subscribers_number: number;
+  subscribers: object[];
+  favorite_songs_number: number;
+  favorite_songs: object[];
+  added_chords_number: number;
+  rating: number;
+}
