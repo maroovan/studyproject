3fda63fee2894e1c7e7f6ef2c3a129a5a40a2ea9
maroovan <maroovan@gmail.com>
2017-12-10 23:14:19 +0300

    Макет андройд приложения

---
+++ android/DreamTeamApplication/app/src/main/java/com/dreamproject/dreamteam/dreamteamapplication/AnimalNames.java
@@ -0,0 +1,18 @@
+package com.dreamproject.dreamteam.dreamteamapplication;
+
+/**
+ * Created by MSI on 27.11.2017.
+ */
+
+public class AnimalNames {
+    private String animalName;
+
+    public AnimalNames(String animalName) {
+        this.animalName = animalName;
+    }
+
+    public String getAnimalName() {
+        return this.animalName;
+    }
+
+}
